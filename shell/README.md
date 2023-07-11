5979b4a2f685886bcaf8d5ad49e474c57a400495
Zack Brady <zackbrady123@gmail.com>
2023-07-11 01:14:41 -0400

    update script deployment and readme

--- shell/README.md
+++ shell/README.md
@@ -1,17 +1,31 @@
-## Carbide Deployment with Script
 
-In order to deploy Rancher Government Carbide, you must following the steps below:
+## Carbide Deployment with Fleet
+
+### Deployment Options
+
+In order to deploy Rancher Government Carbide, you have two deployment options. You can deploy it **only** on the **`local` cluster** or on **all** the **`local` and `downstream` clusters**.
 * Ensure the cluster(s) are configured with the Carbide images and private registry ([docs here](https://rancherfederal.github.io/carbide-docs/docs/registry-docs/kubernetes-config)).
-* Apply the exported variables, while replacing `$DOMAIN`, `$CarbideRegistry`, and `$CarbideLicense`.
-* Lastly, apply the curl command for each cluster(s).
+* Apply the shell variables, while replacing `$DOMAIN`, `$CarbideRegistry`, and `$CarbideLicense`.
+* Lastly, apply the curl command to each of the `local` or `downstream` cluster(s).
 
-### Carbide Script
+### Carbide Local Script
 ```bash
 ### Set Script Variables
 export DOMAIN=domain.url
 export CarbideRegistry=registry.url
 export CarbideLicense=license
 
-### Run the Carbide Script
-curl -#OL https://raw.githubusercontent.com/zackbradys/rgs-carbide/main/shell/carbide-apps.sh
+### Run the Carbide Local Script
+curl -#OL https://raw.githubusercontent.com/zackbradys/rgs-carbide/main/shell/carbide-local.sh
+```
+
+### Carbide Default Script
+```bash
+### Set Script Variables
+export DOMAIN=domain.url
+export CarbideRegistry=registry.url
+export CarbideLicense=license
+
+### Run the Carbide Default Script
+curl -#OL https://raw.githubusercontent.com/zackbradys/rgs-carbide/main/shell/carbide-default.sh
 ```
---
+++ shell/carbide-default.sh
@@ -0,0 +1,42 @@
+### Add Carbide Chart Repo
+kubectl apply -f - <<EOF
+apiVersion: catalog.cattle.io/v1
+kind: ClusterRepo
+metadata:
+  name: carbide-charts
+spec:
+  gitBranch: main
+  gitRepo: https://github.com/rancherfederal/carbide-charts
+EOF
+
+### Create Kubernetes Namespaces
+kubectl create namespace cattle-monitoring-system
+kubectl create namespace cis-operator-system
+kubectl create namespace carbide-docs-system
+kubectl create namespace carbide-stigatron-system
+
+### Install Rancher Monitoring and CRD
+helm upgrade -i rancher-monitoring-crd rancher-charts/rancher-monitoring-crd -n cattle-monitoring-system --version=102.0.1+up40.1.2 --set global.cattle.systemDefaultRegistry=$CarbideRegistry
+
+sleep 10
+
+helm upgrade -i rancher-monitoring rancher-charts/rancher-monitoring -n cattle-monitoring-system --version=102.0.1+up40.1.2 --set global.cattle.systemDefaultRegistry=$CarbideRegistry
+
+### Install CIS Benchmarks and CRD
+helm upgrade -i rancher-cis-benchmark-crd rancher-charts/rancher-cis-benchmark-crd -n cis-operator-system --version=4.0.0 --set global.cattle.url=https://rancher.$DOMAIN --set global.cattle.systemDefaultRegistry=$CarbideRegistry
+
+sleep 10
+
+helm upgrade -i rancher-cis-benchmark rancher-charts/rancher-cis-benchmark -n cis-operator-system --version=4.0.0 --set global.cattle.url=https://rancher.$DOMAIN --set global.cattle.systemDefaultRegistry=$CarbideRegistry
+
+sleep 30
+
+### Install Carbide License
+kubectl create secret generic stigatron-license -n carbide-stigatron-system --from-literal=license=$CarbideLicense
+
+### Install Carbide Applications
+helm repo add carbide-charts https://rancherfederal.github.io/carbide-charts
+helm repo add kubewarden https://charts.kubewarden.io
+helm repo update
+
+helm upgrade -i stigatron carbide-charts/stigatron -n carbide-stigatron-system --set global.cattle.systemDefaultRegistry=$CarbideRegistry --set heimdall2.heimdall.rcidf.registry=$CarbideRegistry --set heimdall2.global.cattle.systemDefaultRegistry=$CarbideRegistry
